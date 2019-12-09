5158a74ae7d28a6da285f41cccff5d56b1a297ac
BergerMarcin <marcin.berger@wp.pl>
2019-12-09 15:35:03 +0100

    entities: User & Role

---
+++ src/main/java/honestit/akwilina/promises/domain/entities/Role.java
@@ -0,0 +1,22 @@
+package honestit.akwilina.promises.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+@Getter @Setter @ToString @EqualsAndHashCode(of = "id")
+public class Role {
+
+    @Id
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
